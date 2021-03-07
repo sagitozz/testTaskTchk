b8a360d05ffec623cdb8dc025f9f02549e3e50c4
sagitozz <sagitopwnz@gmail.com>
2021-03-07 20:12:14 +0300

    initial commit

---
+++ app/src/main/java/com/testtask/testtasktchk/GoogleAuthHelper.kt
@@ -0,0 +1,24 @@
+package com.testtask.testtasktchk
+
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+/**
+ * @autor d.snytko
+ */
+class GoogleAuthHelper(context: Context) {
+
+    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .build()
+
+    private var mGoogleSignInClient : GoogleSignInClient = GoogleSignIn.getClient(context, gso)
+
+    fun getIntent () : Intent {
+        return mGoogleSignInClient.signInIntent
+    }
+}
+
--- app/src/main/java/com/testtask/testtasktchk/MainActivity.kt
+++ app/src/main/java/com/testtask/testtasktchk/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.testtask.testtasktchk
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var googleAuthHelper: GoogleAuthHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        googleAuthHelper = GoogleAuthHelper(this)
+
+        findViewById<View>(R.id.sign_in_button).setOnClickListener {
+            signInGoogle()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_GOOGLE_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+
+                if (result != null) {
+                    if (result.isSuccess) {
+                        result.signInAccount?.let {
+                            Toast.makeText(this, result.signInAccount!!.displayName, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            } else {
+            //TODO обработать результат
+            }
+        }
+    }
+
+    private fun signInGoogle() {
+        val intent = googleAuthHelper.getIntent()
+
+        startActivityForResult(intent, SIGN_GOOGLE_REQUEST)
+    }
+
+    companion object {
+        const val SIGN_GOOGLE_REQUEST = 111
     }
 }
